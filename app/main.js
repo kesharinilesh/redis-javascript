dff55d34bba4f21a59850d67f2f5c415a33d8290
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-01-31 00:06:31 +0530

    stage 5

--- app/main.js
+++ app/main.js
@@ -2,7 +2,29 @@ const net = require("net");
 const server = net.createServer((connection) => {
   // Handled connection
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    // connection.write("+PONG\r\n");
+    const command = data.toString().split("\r\n");
+    console.log(command);
+    switch (command[2].toUpperCase()) {
+      case "PING" || "ping":
+        connection.write("+PONG\r\n");
+        break;
+      case "ECHO":
+        // if the command has the expected format
+        if (command.length >= 4) {
+          // Send the echoed string back to the client
+          connection.write("+" + command[4] + "\r\n");
+        } else {
+          // If the command doesn't have the expected format, send an error message
+          connection.write(
+            "-ERR wrong number of arguments for 'echo' command\r\n"
+          );
+        }
+        break;
+      default:
+        connection.write("-ERR unknown command\r\n");
+        break;
+    }
   });
 
 });
