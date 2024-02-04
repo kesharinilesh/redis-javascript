5cd37e4188085157c30eb7eb5a4cbbbae72f067f
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-02-04 17:03:02 +0530

    stage 6

--- app/main.js
+++ app/main.js
@@ -1,32 +1,36 @@
 const net = require("net");
-const server = net.createServer((connection) => {
-  // Handled connection
-  connection.on("data", (data) => {
-    // connection.write("+PONG\r\n");
-    const command = data.toString().split("\r\n");
-    console.log(command);
-    switch (command[2].toUpperCase()) {
-      case "PING" || "ping":
-        connection.write("+PONG\r\n");
-        break;
-      case "ECHO":
-        // if the command has the expected format
-        if (command.length >= 4) {
-          // Send the echoed string back to the client
-          connection.write("+" + command[4] + "\r\n");
-        } else {
-          // If the command doesn't have the expected format, send an error message
-          connection.write(
-            "-ERR wrong number of arguments for 'echo' command\r\n"
-          );
-        }
-        break;
-      default:
-        connection.write("-ERR unknown command\r\n");
-        break;
-    }
-  });
 
+let DATASTORE = new Map();
+let commandMessage = "PONG";
+
+const server = net.createServer();
+server.on("connection", (socket) => {
+  socket.on("data", (data) => {
+    
+    let { commandName, key, value } = parseData(data);
+    switch(commandName){
+			case "echo":
+				commandMessage = key;
+				break;
+			case "set":
+				DATASTORE.set(key, value);
+				commandMessage = 'OK';
+				break;
+			case "get":
+				let storeValue = DATASTORE.get(key);
+				commandMessage = !storeValue ? null : storeValue;
+				break;
+		}
+    
+    socket.write(`+${commandMessage}\r\n`);
+  });
 });
+const parseData = (request) => {
+	let data = request.toString().split("\r\n");
+  let commandName = data[2];
+  let key = data[4];
+  let value = data[6];
+	return { commandName, key, value }
+};
 
 server.listen(6379, "127.0.0.1");
